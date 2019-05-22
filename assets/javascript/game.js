988032fe3ac4af9be05654942f3287dbc2049f5d
ToneDef00 <daniels@dual4t.com>
2019-05-22 12:15:36 -0700

    started using class system and begin creating constructors for the game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 
-var getRandomInt = function(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+let getRandomInt = function (max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 //declare global variables
 //player wins 
@@ -9,20 +9,83 @@ var getRandomInt = function(max) {
 //letters guessed 
 //number of guesses left
 //
-var words = ["coursework","bootcamp","javascript","cascading", "hello", "computer","icecream","pizza"];
-var currentWord = words[7];
+const gameWords = ["coursework", "bootcamp", "javascript", "cascading", "hello", "computer", "icecream", "pizza"];
+const idRange = ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", 'a10', "a11", "a12", "a13", "a14", "a15"];
+let currentWord = words[7];
 
 //array of letterstructs 
-var current = [];
 
-var pastGuess=[];
+
+var pastGuess = [];
 
 //function for line 27
 
- function letterstruct(input) {
-  this.letter = input;
-  this.show = false;
+//  const Letterstruct = function(input, id) {
+//   this.letter = input;
+//   this.show = false;
+//   this.ID = id;
+// }
+
+class Letterstruct {
+  constructor(input, elemID) {
+    this.letter = input;
+    this.ID = elemID;
+    //variables automatically set by constructor 
+    this.show = false;
+    this.blank = "___";
+
+  }
+
+  render() {
+    if (this.show) {
+      document.getElementById(this.ID).innerHTML = this.letter;
+    }
+    document.getElementById(this.ID).innerHTML = this.blank;
+  }
+
+  show() {
+    show = true;
+    this.render()
+  }
+
+  get elementID() {
+    return this.ID
+  }
+
+
 }
+// console.log(`stuff to talk about ${this.letter}`)
+// use the key to the left of one. called backticks 
+
+//a class which takes a word and idRange and returns an object containing an array of letterstructs and number of letters and some rendering functions
+class current {
+  constructor(word, idArray) {
+    this.letterArray = [];
+    for (let i = 0; i < word.length; i++) {
+      this.letterArray.push(new letterstruct(word[i], idArray));
+    }
+    this.numberOfletters = word.length;
+
+  }
+
+  //function which itterates through each letter and renders them based on the show state (true/false)
+  renderAll() {
+    this.letterArray.forEach(function (elem) {
+      elem.render();
+    });
+  }
+
+  //function which iterates through each letter and renders all the elements which corespond to the current guess
+  renderLetter(l) {
+    this.letterArray.forEach(function (elem) {
+      if (elem.letter == l) {
+        elem.show();
+      }
+    });
+  }
+
+}
+
 
 
 currentWord.forEach(letter => {
@@ -44,5 +107,5 @@ console.log(current);
 //listen for key presses and update the 
 document.onkeyup = function (event) {
 
-    
+
 }
