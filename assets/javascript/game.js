f3cafeeec07c8ca706b2c59212a9abb13c26febf
ToneDef00 <daniels@dual4t.com>
2019-05-22 19:16:53 -0700

    fixed var declared already when its only declared once bug. fixed ID matching bug by moving the javascript linking to the end of the body. Debugged the letterstruct class and tested the render function. Render function will display depending on boolean value change by show.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,62 +10,88 @@ let getRandomInt = function (max) {
 //number of guesses left
 //
 const gameWords = ["coursework", "bootcamp", "javascript", "cascading", "hello", "computer", "icecream", "pizza"];
-const idRange = ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", 'a10', "a11", "a12", "a13", "a14", "a15"];
-let currentWord = gameWords[7];
+const idRange = [
+  {A: document.getElementById("a1")},
+  {A: document.getElementById("a2")},
+  {A: document.getElementById("a3")},
+  {A: document.getElementById("a4")},
+  {A: document.getElementById("a5")},
+  {A: document.getElementById("a6")},
+  {A: document.getElementById("a7")},
+  {A: document.getElementById("a8")},
+  {A: document.getElementById("a9")},
+  {A: document.getElementById("a10")},
+  {A: document.getElementById("a11")},
+  {A: document.getElementById("a12")}
+];
 
-//array of letterstructs 
+console.log(idRange[1].ID);
+console.log("_________________________________");
+
+var currentWord = gameWords[7];
 
 
-var pastGuess = [];
 
-//function for line 27
 
-//  const Letterstruct = function(input, id) {
-//   this.letter = input;
-//   this.show = false;
-//   this.ID = id;
-// }
-
-//Letterstruct is a class which takes an input letter and element ID and constructs 
+// //Letterstruct is a class which takes an input letter and element ID from getelementbyid() and constructs letter object with render methods
+//letter
+//ID
+//hidden Set True by default 
+//blank set to "___"
+// render() renders the letter as based on the hidden state
+//show() changed the hidden to false and renders() 
 class Letterstruct {
   constructor(input, elemID) {
     this.letter = input;
     this.ID = elemID;
     //variables automatically set by constructor 
-    this.show = false;
+    this.hidden = true;
     this.blank = "___";
 
+    
   }
 
   render() {
-    if (this.show) {
-      document.getElementById(this.ID).innerHTML = this.letter;
+    console.log("Rendering page")
+    if (this.hidden == false) {
+      this.ID.textContent = this.letter;
+      return;
     }
-    document.getElementById(this.ID).innerHTML = this.blank;
+
+    this.ID.textContent = this.blank;
   }
 
-  show() {
-    show = true;
-    this.render()
-  }
-
-  get elementID() {
-    return this.ID
+  show(){
+    console.log("showing letterstruct value");
+    this.hidden = false;
+    this.render();
   }
 
 
 }
+
+const newLetter = new Letterstruct("h", idRange[0].A);
+console.log(newLetter.render());
+
 // console.log(`stuff to talk about ${this.letter}`)
 // use the key to the left of one. called backticks 
 
-//a class which takes a word and idRange and returns an object containing an array of letterstructs and number of letters and some rendering functions
-class current {
+//Primary game state class contains a constructor which takes a word in string form and an array of id objects
+// letterArray is a constructed array of letterstructs
+// Number of letters is the total number of characters in the word
+//guessesRemaining = 1.8 times the number of characters
+class GameState {
   constructor(word, idArray) {
     this.letterArray = [];
     for (let i = 0; i < word.length; i++) {
-      this.letterArray.push(new Letterstruct(word[i], idArray[i]));
+      this.letterArray.push(new Letterstruct(word[i], idArray[i].A));
+      // letterArray[]
     }
     this.numberOfletters = word.length;
+    this.guessesRemaining = word.length*1.8;
+    this.pastGuess = [];
+    this.wins = 0;
+    this.losses = 0;
 
   }
 
@@ -88,9 +114,10 @@ class current {
 }
 ''
 
-const newLetter = new Letterstruct("h", "a1")
+const newGameState = new GameState("hellllagood",idRange);
+console.log("newGameState succseful");
 
-newLetter.show();
+newGameState.renderLetter("l");
 
 
 // 
@@ -104,7 +131,7 @@ newLetter.show();
 
 
 //listen for key presses and update the 
-document.onkeyup = function (event) {
+// document.onkeyup = function (event) {
 
 
-}
+// }
